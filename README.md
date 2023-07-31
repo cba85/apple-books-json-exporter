afebe0d4f7c9f81d3524d7964bb1868242c678fa
Clément Barbaza <clement.barbaza@gmail.com>
2023-07-31 03:02:02 +0200

    add collections and assets in annotations

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
 # Apple Books Exporter
 
+Export Apple Books in json.
+
+This script exports:
+
+* Your books (assets)
+* Your collections of books
+* Your annotations (and books linked)
+
+## Install
+
+```
+$ npm install
+```
+
+## Usage
+
+```
+$ node main.js
+```
+
 ## Ressources
 
 * https://github.com/atfzl/obsidian-apple-books-plugin
--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 import fs from "fs/promises";
 import Db from "better-sqlite3";
+import slugify from "slugify";
 
-const BOOKS_DATABASE_DIRECTORY = `${process.env.HOME}/Library/Containers/com.apple.iBooksX/Data/Documents/BKLibrary`;
+const ASSETS_DATABASE_DIRECTORY = `${process.env.HOME}/Library/Containers/com.apple.iBooksX/Data/Documents/BKLibrary`;
 const ANNOTATIONS_DATABASE_DIRECTORY = `${process.env.HOME}/Library/Containers/com.apple.iBooksX/Data/Documents/AEAnnotation`;
-
-let booksDatabaseFile = "";
-let annotationsDatabaseFile = "";
 
 const getDatabaseFile = async (directory) => {
   let databaseFile;
+  let files;
 
-  const files = await fs.readdir(directory);
+  try {
+    files = await fs.readdir(directory);
+  } catch (e) {
+    process.exit(1);
+  }
 
   for (const file of files) {
     const fileFragments = file.split(".");
@@ -25,13 +28,16 @@ const getDatabaseFile = async (directory) => {
 };
 
 (async () => {
-  booksDatabaseFile = await getDatabaseFile(BOOKS_DATABASE_DIRECTORY);
-  annotationsDatabaseFile = await getDatabaseFile(
-    ANNOTATIONS_DATABASE_DIRECTORY
-  );
+  // ASSETS
+  const assetsDatabaseFile = await getDatabaseFile(ASSETS_DATABASE_DIRECTORY);
 
-  const dbBooks = new Db(booksDatabaseFile);
-  const books = dbBooks
+  if (!assetsDatabaseFile) {
+    process.exit(1);
+  }
+
+  const dbAssets = new Db(assetsDatabaseFile);
+
+  const assets = dbAssets
     .prepare(
       `SELECT
       ZASSETID as id, ZTITLE AS title, ZAUTHOR AS author, ZLANGUAGE as language, ZPATH as path
@@ -40,18 +46,98 @@ const getDatabaseFile = async (directory) => {
     )
     .all();
 
+  const collections = dbAssets
+    .prepare(
+      `SELECT
+      ZCOLLECTIONID as id, ZTITLE AS title, Z_PK as pk
+      FROM ZBKCOLLECTION
+      WHERE Z_PK > 8`
+    )
+    .all();
+
+  const members = dbAssets
+    .prepare(
+      `SELECT
+    ZCOLLECTION as id, ZASSETID as assetId
+    FROM ZBKCOLLECTIONMEMBER
+    WHERE ZCOLLECTION > 8`
+    )
+    .all();
+
+  // COLLECTIONS
+
+  for (const collection of collections) {
+    const assetsInCollection = {
+      id: collection.id,
+      pk: collection.pk,
+      title: collection.title,
+      members: [],
+    };
+
+    const collectionMembers = members.filter((m) => {
+      return m.id == collection.pk;
+    });
+
+    for (const member of collectionMembers) {
+      const asset = assets.filter((a) => {
+        return a.id == member.assetId;
+      });
+
+      if (asset) {
+        assetsInCollection.members.push(asset);
+      }
+    }
+
+    await fs.writeFile(
+      `./export/${slugify(collection.title)}.json`,
+      JSON.stringify(assetsInCollection)
+    );
+  }
+
+  await fs.writeFile("./export/assets.json", JSON.stringify(assets));
+
+  // ANNOTATIONS
+
+  const annotationsDatabaseFile = await getDatabaseFile(
+    ANNOTATIONS_DATABASE_DIRECTORY
+  );
+
+  if (!annotationsDatabaseFile) {
+    process.exit(1);
+  }
+
   const dbAnnotations = new Db(annotationsDatabaseFile);
+
   const annotations = dbAnnotations
     .prepare(
       `SELECT
-      ZANNOTATIONASSETID as assetId, ZANNOTATIONUUID as uuid, ZANNOTATIONSELECTEDTEXT as selectedText, ZFUTUREPROOFING5 as Chapter, ZANNOTATIONCREATIONDATE as Created, ZANNOTATIONMODIFICATIONDATE as Modified
+      ZANNOTATIONASSETID as assetId, ZANNOTATIONSELECTEDTEXT as selectedText, ZFUTUREPROOFING5 as chapter, ZANNOTATIONCREATIONDATE as creationDate, ZANNOTATIONMODIFICATIONDATE as modificationDate
       FROM ZAEANNOTATION
       WHERE ZANNOTATIONDELETED = 0 AND ZANNOTATIONSELECTEDTEXT NOT NULL`
     )
     .all();
 
-  await fs.writeFile("./export/books.json", JSON.stringify(books));
-  await fs.writeFile("./export/annotations.json", JSON.stringify(annotations));
+  const annotationsBook = [];
+
+  for (const annotation of annotations) {
+    const asset = assets.filter((a) => {
+      return a.id == annotation.assetId;
+    });
+
+    annotationsBook.push({
+      assetId: annotation.assetId,
+      selectedText: annotation.selectedText,
+      chapter: annotation.chapter,
+      creationDate: annotation.creationDate,
+      modificationDate: annotation.modificationDate,
+      asset: asset,
+    });
+  }
+
+  await fs.writeFile(
+    "./export/annotations.json",
+    JSON.stringify(annotationsBook)
+  );
 
   process.exit();
 })();
